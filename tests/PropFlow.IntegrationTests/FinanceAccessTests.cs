30935e398cc0f3457658d4fdc5f7f810a8fc4f60
Villitaryy <nguyenlehuyhungka@gmail.com>
2026-09-18 22:15:49 +0700

    fix navigation UI Accountant

--- tests/PropFlow.IntegrationTests/FinanceAccessTests.cs
+++ tests/PropFlow.IntegrationTests/FinanceAccessTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
+using PropFlow.Web.Client.Layout;
 using PropFlow.Web.Client.Services.Authentication;
 
 namespace PropFlow.IntegrationTests;
@@ -22,5 +23,19 @@ public sealed class FinanceAccessTests
             Assert.NotNull(authorization);
             Assert.Equal("ACCOUNTANT", authorization.Roles);
         });
+    }
+
+    [Fact]
+    public void Finance_pages_use_accountant_layout()
+    {
+        var pages = typeof(AuthSession).Assembly.GetTypes()
+            .Where(type => type.GetCustomAttributes(typeof(RouteAttribute), false)
+                .Cast<RouteAttribute>().Any(route => route.Template.StartsWith("/finance/", StringComparison.Ordinal)))
+            .ToArray();
+
+        Assert.Equal(5, pages.Length);
+        Assert.All(pages, page =>
+            Assert.Equal(typeof(AccountantLayout), page.GetCustomAttributes(typeof(LayoutAttribute), false)
+                .Cast<LayoutAttribute>().SingleOrDefault()?.LayoutType));
     }
 }
